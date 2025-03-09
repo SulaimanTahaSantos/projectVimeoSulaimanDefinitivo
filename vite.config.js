3b597d25106cdd70f02f5f7b9fb670ccf2fbc0f4
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-03-09 23:45:51 +0100

    aaafafaw

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
- root: path.resolve(__dirname, 'src'),
- publicDir: path.resolve(__dirname, 'public'),
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
+  root: path.resolve(__dirname, 'src'),
+  publicDir: path.resolve(__dirname, 'public'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'), // Asegura que el build se guarde en dist
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
+  base: './', // 🔥 IMPORTANTE para que las rutas sean relativas
 });
